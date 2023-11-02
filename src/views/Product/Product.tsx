d45e1b99a1d740fcc597198346eb6efa3c15b3a4
Larre Ländin <3859057+larrelandin@users.noreply.github.com>
2023-11-02 11:29:04 +0100

    Updated destination version, switched to vercel and minor changes to product page

--- src/views/Product/Product.tsx
+++ src/views/Product/Product.tsx
@@ -63,12 +63,13 @@ const Product = () => {
           <section className="text-gray-700 body-font overflow-hidden bg-white">
             <div className="container px-5 py-24 mx-auto">
               <div className="lg:w-4/5 mx-auto flex flex-wrap">
-                <img alt="ecommerce" className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src={product.thumbnail} />
+                <img alt="ecommerce" className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src={product.media && product.media[0] && product.media[0].url !== null ? product.media![0].url : ''} />
                 <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                   <h2 className="text-sm title-font text-gray-500 tracking-widest">{product.collection}</h2>
                   <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{product.name}</h1>
                   <div className="flex mb-4">
                     <span className="flex items-center">
+                    <span className="text-gray-600 mr-2">{product.averageReview}</span>
                       <svg fill={product.averageReview! >= 0.5 ? 'currentColor' : 'none'} stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-red-500" viewBox="0 0 24 24">
                         <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                       </svg>
@@ -106,7 +107,7 @@ const Product = () => {
                   </div>
                   <p className="leading-relaxed">{product.description}</p>
                   <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
-                    <div className="flex">
+                    {/* <div className="flex">
                       <span className="mr-3">Color</span>
                       <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
                       <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
@@ -127,6 +128,16 @@ const Product = () => {
                           </svg>
                         </span>
                       </div>
+                    </div> */}
+                  </div>
+                  <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
+                    <div className="flex">
+                      {product.wash}
+                    </div>
+                  </div>
+                  <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
+                    <div className="flex">
+                      {product.material}
                     </div>
                   </div>
                   <div className="flex">
