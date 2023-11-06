0ac7a00231d72260cad60951cceda66fc735ca54
Larre Ländin <3859057+larrelandin@users.noreply.github.com>
2023-11-06 13:16:19 +0100

    Added image gallery

--- src/views/Product/Product.tsx
+++ src/views/Product/Product.tsx
@@ -4,6 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 import { useFilter } from '@/providers/FilterProvider';
 import {useParams} from 'react-router-dom';
 import Accordion from '../components/Accordion';
+import ImageGallery from 'react-image-gallery';
 
 type Increment<A extends number[]> = [...A, 0];
  
@@ -64,7 +65,17 @@ const Product = () => {
           <section className="text-gray-700 body-font overflow-hidden bg-white">
             <div className="container px-5 py-24 mx-auto">
               <div className="lg:w-4/5 mx-auto flex flex-wrap">
-                <img alt="ecommerce" className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src={product.media && product.media[0] && product.media[0].url !== null ? product.media![0].url : ''} />
+                <div className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200">
+                  <ImageGallery
+                    showBullets={false}
+                    showFullscreenButton={false}
+                    showPlayButton={false}
+                    items={product.media!.slice(1).filter(item => item.url && item.url.trim() !== '').map(item => ({
+                      original: item.url || '',
+                      thumbnail: item.url + '?impolicy=small'
+                    }))}
+                  />
+                  </div>
                 <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                   <h2 className="text-sm title-font text-gray-500 tracking-widest">{product.collection}</h2>
                   <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{product.name}</h1>
@@ -88,7 +99,7 @@ const Product = () => {
                       </svg>
                       <span className="text-gray-600 ml-3">{product.totalReviews} Reviews</span>
                     </span>
-                    <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
+                    {/* <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
                       <a className="text-gray-500">
                         <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
                           <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
@@ -104,7 +115,7 @@ const Product = () => {
                           <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                         </svg>
                       </a>
-                    </span>
+                    </span> */}
                   </div>
                   <p className="leading-relaxed">{product.description}</p>
                   <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
@@ -136,12 +147,12 @@ const Product = () => {
                   <Accordion id='weight' title= "Weight" text= {product.weight} />
                   <div className="flex">
                     <span className="title-font font-medium text-2xl text-gray-900">{product.pricing && product.pricing[0] && product.pricing[0].price !== null ? product.pricing[0].price + ' ' + product.pricing[0].currency : ''}</span>
-                    <button className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Button</button>
-                    <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                    <button className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Commerce!</button>
+                    {/* <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                       <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
                         <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
                       </svg>
-                    </button>
+                    </button> */}
                   </div>
                 </div>
               </div>
