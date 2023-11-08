7516c23459f53c2dac1e29df3b056ed62774034e
Larre Ländin <3859057+larrelandin@users.noreply.github.com>
2023-11-08 08:54:29 +0100

    "Name" removed from destination, using "title" instead

--- src/views/Product/Product.tsx
+++ src/views/Product/Product.tsx
@@ -80,7 +80,7 @@ const Product = () => {
                   </div>
                 <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                   <h2 className="text-sm title-font text-gray-500 tracking-widest">{product.collection}</h2>
-                  <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{product.name}</h1>
+                  <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{product.title}</h1>
                   <div className="flex mb-4">
                     <span className="flex items-center">
                       <span className="text-gray-600 mr-2">{product.averageReview}</span>
@@ -101,11 +101,6 @@ const Product = () => {
                   <div className="flex">
                     <span className="title-font font-medium text-2xl text-gray-900">{product.pricing && product.pricing[0] && product.pricing[0].price !== null ? product.pricing[0].price + ' ' + product.pricing[0].currency : ''}</span>
                     <button className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Commerce!</button>
-                    {/* <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                      <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                        <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                      </svg>
-                    </button> */}
                   </div>
                 </div>
               </div>
